8c5ee91f1b8e5a28b11e01537e87ea81166f1c2f
NihongoMod <minecraft_nihongo_mod@hotmail.co.jp>
2012-05-03 21:57:54 +0900

    fix tabs

--- net/minecraft/src/mod_BitmapFont.java
+++ net/minecraft/src/mod_BitmapFont.java
@@ -12,64 +12,61 @@ import net.minecraft.src.bmpfont.BitmapFontRenderer;
 
 /**
  * ビットマップフォントを表示するためのMOD
- * 
- * @author wiro
  */
 public class mod_BitmapFont extends BaseMod {
-	public static final String UNIQUE_NAME = "BitmapFont";
-	public static final String VERSION = "v1.0";
+    public static final String UNIQUE_NAME = "BitmapFont";
+    public static final String VERSION = "v1.0";
 
-	private BMPFont_FontRenderer fontRenderer;
-	private BitmapFontProperties properties;
+    private BMPFont_FontRenderer fontRenderer;
+    private BitmapFontProperties properties;
 
-	@Override
-	public String getVersion() {
-		return UNIQUE_NAME + " " + VERSION;
-	}
+    @Override
+    public String getVersion() {
+        return UNIQUE_NAME + " " + VERSION;
+    }
 
-	public mod_BitmapFont() throws IOException {
-		File mcroot = Minecraft.getMinecraftDir();
-		File configDir = new File(mcroot, "config");
-		File propFile = new File(configDir, UNIQUE_NAME + ".properties");
-		properties = new BitmapFontProperties(propFile);
-		
-		File fontFile = new File(getFontDir(mcroot), properties.fontFile);
-		BitmapFontLoader loader = new BitmapFontLoader();
-		BitmapFont font = loader.load(fontFile);
+    public mod_BitmapFont() throws IOException {
+        File mcroot = Minecraft.getMinecraftDir();
+        File configDir = new File(mcroot, "config");
+        File propFile = new File(configDir, UNIQUE_NAME + ".properties");
+        properties = new BitmapFontProperties(propFile);
 
-		Minecraft minecraft = ModLoader.getMinecraftInstance();
-		BitmapFontRenderer renderer = new BitmapFontRenderer(minecraft, font, (float)properties.baseLine, (float)properties.scale);
-		this.fontRenderer = new BMPFont_FontRenderer(renderer, minecraft.gameSettings, "/font/default.png", minecraft.renderEngine, false);
+        File fontFile = new File(getFontDir(mcroot), properties.fontFile);
+        BitmapFontLoader loader = new BitmapFontLoader();
+        BitmapFont font = loader.load(fontFile);
 
-		this.fontRenderer.setEnableMod(properties.enableMod);
-		
-		// FontRendererの置き換え
-		minecraft.fontRenderer = this.fontRenderer;
-	}
-	
-	/**
-	 * font/ ディレクトリを取得します。
-	 */
-	private File getFontDir(File mcroot) {
-		File fontDir = new File(getResourceDir(mcroot), "font");
-    	if (!fontDir.exists())
-    		fontDir.mkdir();
-    	return fontDir;
-	}
+        Minecraft minecraft = ModLoader.getMinecraftInstance();
+        BitmapFontRenderer renderer = new BitmapFontRenderer(minecraft, font, (float) properties.baseLine, (float) properties.scale);
+        this.fontRenderer = new BMPFont_FontRenderer(renderer, minecraft.gameSettings, "/font/default.png", minecraft.renderEngine, false);
 
-	/**
-	 * resources/ フォルダを取得します。
-	 * 
-	 * @param mcroot MCROOT
-	 * @return
-	 */
-	private File getResourceDir(File mcroot) {
-    	File rsrcDir = new File(mcroot, "resources");
-    	if (!rsrcDir.exists())
-    		rsrcDir.mkdir();
-    	return rsrcDir;
-	}
-	
-	@Override
-	public void load() {}
+        this.fontRenderer.setEnableMod(properties.enableMod);
+
+        // FontRendererの置き換え
+        minecraft.fontRenderer = this.fontRenderer;
+    }
+
+    /**
+     * font/ ディレクトリを取得します。
+     */
+    private File getFontDir(File mcroot) {
+        File fontDir = new File(getResourceDir(mcroot), "font");
+        if (!fontDir.exists())
+            fontDir.mkdir();
+        return fontDir;
+    }
+
+    /**
+     * resources/ フォルダを取得します。
+     * @return
+     */
+    private File getResourceDir(File mcroot) {
+        File rsrcDir = new File(mcroot, "resources");
+        if (!rsrcDir.exists())
+            rsrcDir.mkdir();
+        return rsrcDir;
+    }
+
+    @Override
+    public void load() {
+    }
 }
